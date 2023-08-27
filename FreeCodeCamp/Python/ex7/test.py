5cc955654dc094ea7b9ad92da37321d237d71b07
Josh-dev69 <ebubejoshua69@gmail.com>
2023-08-27 09:34:34 +0100

    Still Practicing

--- FreeCodeCamp/Python/ex7/test.py
+++ FreeCodeCamp/Python/ex7/test.py
@@ -1,8 +1,31 @@
-word = 'brontosaurus'
-d = dict()
-for c in word:
-    if c not in d:
-        d[c] = 1
-    else:
-        d[c] = d[c] + 1
-print(d)
+# word = 'brontosaurus'
+#d = dict()
+#for c in word:
+#    if c not in d:
+#        d[c] = 1
+#    else:
+#        d[c] = d[c] + 1
+#print(d)
+
+#e = dict()
+#for c in word:
+#    e[c] = e.get(c,0) + 1
+#print(e)
+
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+
+counts = dict()
+for line in fhand:
+    words = line.split()
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+
+print(counts)
